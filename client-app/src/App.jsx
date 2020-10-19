735c38bfaa0a8450622028da64b7598690f1122d
Robert <robert@primalvinyl.com>
2020-10-19 12:23:02 -0700

    memoizes breakpointprovider component

--- client-app/src/App.jsx
+++ client-app/src/App.jsx
@@ -9,7 +9,6 @@ function App() {
             <BreakpointProvider>
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
-                    <p>Edit <code>src/App.js</code> and save to reload.</p>
                 </header>
             </BreakpointProvider>
         </div>
--- client-app/src/utilities/BreakpointProvider.js
+++ client-app/src/utilities/BreakpointProvider.js
@@ -14,19 +14,19 @@ const BreakpointContext = React.createContext({
 
 const BreakpointProvider = ({ children }) => {
     const [queryMatch, setQueryMatch] = React.useState({});
-    const queries = {
-        'xs': '(max-width: 599px)',
-        'smUp': '(min-width: 600px)',
-        'mdUp': '(min-width: 900px)',
-        'lgUp': '(min-width: 1200px)',
-        'xlUp': '(min-width: 1800px)',
-        'smDown': '(max-width: 600px)',
-        'mdDown': '(max-width: 900px)',
-        'lgDown': '(max-width: 1200px)',
-        'xlDown': '(max-width: 1800px)'
-    };
 
-    React.useEffect(() => {
+    const getMediaQuery = React.useCallback(() => {
+        const queries = {
+            'xs': '(max-width: 599px)',
+            'smUp': '(min-width: 600px)',
+            'mdUp': '(min-width: 900px)',
+            'lgUp': '(min-width: 1200px)',
+            'xlUp': '(min-width: 1800px)',
+            'smDown': '(max-width: 600px)',
+            'mdDown': '(max-width: 900px)',
+            'lgDown': '(max-width: 1200px)',
+            'xlDown': '(max-width: 1800px)'
+        };
         const mediaQueryLists = {};
         const keys = Object.keys(queries);
         let isAttached = false;
@@ -36,7 +36,7 @@ const BreakpointProvider = ({ children }) => {
                 acc[media] = !!(mediaQueryLists[media] && mediaQueryLists[media].matches);
                 return acc;
             }, {})
-            setQueryMatch(updatedMatches)
+            setQueryMatch(updatedMatches);
         };
 
         if (window && window.matchMedia) {
@@ -44,16 +44,16 @@ const BreakpointProvider = ({ children }) => {
             keys.forEach(media => {
                 if (typeof queries[media] === 'string') {
                     mediaQueryLists[media] = window.matchMedia(queries[media]);
-                    matches[media] = mediaQueryLists[media].matches
+                    matches[media] = mediaQueryLists[media].matches;
                 } else {
-                    matches[media] = false
+                    matches[media] = false;
                 }
             });
             setQueryMatch(matches);
             isAttached = true;
             keys.forEach(media => {
                 if (typeof queries[media] === 'string') {
-                    mediaQueryLists[media].addListener(handleQueryListener)
+                    mediaQueryLists[media].addListener(handleQueryListener);
                 }
             });
         }
@@ -62,12 +62,14 @@ const BreakpointProvider = ({ children }) => {
             if (isAttached) {
                 keys.forEach(media => {
                     if (typeof queries[media] === 'string') {
-                        mediaQueryLists[media].removeListener(handleQueryListener)
+                        mediaQueryLists[media].removeListener(handleQueryListener);
                     }
                 });
             }
         };
-    }, [queryMatch, queries]);
+    }, []);
+
+    React.useEffect(() => getMediaQuery(), [getMediaQuery]);
 
     return (
         <BreakpointContext.Provider value={ queryMatch }>
