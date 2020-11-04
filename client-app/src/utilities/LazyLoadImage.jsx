84b51d2e05483f898a61f865d480565b286b4fe9
Robert <robert@primalvinyl.com>
2020-11-04 12:00:10 -0800

    adds image lazy loading

--- client-app/src/utilities/LazyLoadImage.jsx
+++ client-app/src/utilities/LazyLoadImage.jsx
@@ -1,18 +1,52 @@
 import React from 'react';
+import { Transition } from 'react-transition-group';
 import LazyLoad from 'react-lazyload';
 import PropTypes from 'prop-types';
 
-const LazyLoadImage = ({ src, alt, width, height, offset, className }) => {
+const LazyLoadImage = ({
+    src,
+    alt,
+    className,
+    offset,
+    duration
+}) => {
+
+    const [hasLoaded, setHasLoaded] = React.useState(false);
+    const containerStyle = {
+        transition: `opacity ease-in-out ${duration}ms`,
+        opacity: 0,
+        width: '100%'
+    }
+    const imageStyle = {
+        maxWidth: '100%',
+        height: 'auto'
+    };
+    const transitionStyle = {
+        entering: { opacity: 1 },
+        entered: { opacity: 1 },
+        exiting: { opacity: 0 },
+        exited: { opacity: 0 },
+    };
 
     return (
-        <LazyLoad offset={offset}>
-            <img
-                src={src}
-                alt={alt}
-                className={className}
-                width={width}
-                height={height}
-            />
+        <LazyLoad
+            offset={offset}
+            classNamePrefix={className}
+        >
+            <Transition in={hasLoaded} timeout={duration}>
+                {state => (
+                    <div style={{ ...containerStyle, ...transitionStyle[state] }}>
+                        <img
+                            width="3000"
+                            height="3000"
+                            src={src}
+                            alt={alt}
+                            onLoad={() => setHasLoaded(true)}
+                            style={imageStyle}
+                        />
+                    </div>
+                )}
+            </Transition>
         </LazyLoad>
     );
 };
@@ -21,16 +55,15 @@ LazyLoadImage.protoTypes = {
     src: PropTypes.string.isRequired,
     alt: PropTypes.string,
     className: PropTypes.string,
-    width: PropTypes.string,
-    height: PropTypes.string,
+    offset: PropTypes.number,
+    duration: PropTypes.number
 };
 
 LazyLoadImage.defaultProps = {
-    src: '',
     alt: '',
     className: undefined,
-    width: undefined,
-    height: undefined,
+    offset: 150,
+    duration: 300
 };
 
 export default LazyLoadImage;
